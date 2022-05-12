76b497a1cbf1827165a476e293defff8978b82c0
Haruka Ichinose <hic21@uclive.ac.nz>
2022-05-13 03:25:25 +1200

    Finished the styling

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import "./App.css";
-import {useState} from 'react';
+import { useState } from "react";
 
 function App() {
-
-  const [color, setColor] = useState('');
+  const [color, setColor] = useState("");
 
   function handleColorNameInput(colorName) {
     setColor(colorName);
   }
 
   return (
-    <div>
-      <div style={{backgroundColor: `${color}`}}>{color.length > 0 ? color : 'Empty Value'}</div>
-      <form onSubmit={(event) => event.preventDefault()}>
-        <label htmlFor="colorName"></label>
-        <input id="colorName" placeholder="Add color name" type="text" value={color} onChange={(event) => handleColorNameInput(event.target.value)} />
-      </form>
+    <div className="App">
+      <div>
+        <div className="box color-box" style={{ backgroundColor: `${color}` }}>
+          <p>{color.length > 0 ? color : "Empty Value"}</p>
+        </div>
+        <form onSubmit={(event) => event.preventDefault()}>
+          <label htmlFor="colorName"></label>
+          <input
+            id="colorName"
+            className="box input-box"
+            placeholder="Add color name"
+            autoFocus
+            type="text"
+            value={color}
+            onChange={(event) => handleColorNameInput(event.target.value)}
+          />
+        </form>
+      </div>
     </div>
   );
 }
